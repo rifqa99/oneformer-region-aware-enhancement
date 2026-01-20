aebe3b8118d01a04eb24c5ecb77413b341eeeb59
Rifqa <rifqa99@users.noreply.github.com>
2026-01-20 10:20:43 +0300

    Add Day-1 stubs: dataset, OneFormer wrapper, train script

---
+++ src/datasets/dataset.py
@@ -0,0 +1,18 @@
+from torch.utils.data import Dataset
+from PIL import Image
+import torch
+
+
+class ImagePairDataset(Dataset):
+    def __init__(self, image_paths, transform=None):
+        self.image_paths = image_paths
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        img = Image.open(self.image_paths[idx]).convert("RGB")
+        if self.transform:
+            img = self.transform(img)
+        return img
